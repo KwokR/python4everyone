0c86ac1252c26e05afa67fa096920739b65fb7c5
RayKwok33 <raykwok33@gmail.com>
2021-06-27 08:25:49 -0700

    chapter 6 - Strings

---
+++ Chp6_Strings/Chp6_ex1.py
@@ -0,0 +1,11 @@
+"""
+Exercise 1: Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string,
+printing each letter on a separate line, except backwards.
+
+Another way to write a traversal is with a for loop:
+
+for char in fruit:
+    print(char)
+Each time through the loop, the next character in the string is assigned to the variable char. The loop continues until no characters are left.
+
+"""
