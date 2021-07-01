a41ecb1bd0b1adf3019d1ed8f1c65a3b11f1bdfd
RayKwok33 <raykwok33@gmail.com>
2021-06-30 20:01:30 -0700

    chapter 7 - Files

--- Chp7_Files/Chp7_ex2.py
+++ Chp7_Files/Chp7_ex2.py
@@ -2,7 +2,10 @@
 Exercise 2: Write a program to prompt for a file name, and then read through the file and look for lines of the form:
 
 X-DSPAM-Confidence: 0.8475
-When you encounter a line that starts with “X-DSPAM-Confidence:” pull apart the line to extract the floating-point number on the line. Count these lines and then compute the total of the spam confidence values from these lines. When you reach the end of the file, print out the average spam confidence.
+When you encounter a line that starts with “X-DSPAM-Confidence:”
+pull apart the line to extract the floating-point number on the line.
+Count these lines and then compute the total of the spam confidence values from these lines.
+When you reach the end of the file, print out the average spam confidence.
 
 Enter the file name: mbox.txt
 Average spam confidence: 0.894128046745
@@ -11,3 +14,18 @@ Enter the file name: mbox-short.txt
 Average spam confidence: 0.750718518519
 Test your file on the mbox.txt and mbox-short.txt files.
 """
+fname = input('Enter a filename: ')
+try:
+    fhandle = open(fname)
+    searchText = 'X-DSPAM-Confidence: '
+    total = 0.0
+    count = 0
+    for line in fhandle:
+        if line.startswith(searchText):
+            count += 1
+            total += float(line[len(searchText):])
+    average = total/count
+    print('Average spam confidence: ' + str(average))
+except:
+    print(fname, 'is an invalid filename')
+    quit()
--- Chp7_Files/Chp7_ex3.py
+++ Chp7_Files/Chp7_ex3.py
@@ -1,5 +1,10 @@
 """"
-Exercise 3: Sometimes when programmers get bored or want to have a bit of fun, they add a harmless Easter Egg to their program. Modify the program that prompts the user for the file name so that it prints a funny message when the user types in the exact file name “na na boo boo”. The program should behave normally for all other files which exist and don’t exist. Here is a sample execution of the program:
+Exercise 3: Sometimes when programmers get bored or want to have a bit of fun,
+they add a harmless Easter Egg to their program.
+Modify the program that prompts the user for the file name so that it prints a funny message
+when the user types in the exact file name “na na boo boo”.
+The program should behave normally for all other files which exist and don’t exist.
+Here is a sample execution of the program:
 
 python egg.py
 Enter the file name: mbox.txt
@@ -14,3 +19,20 @@ Enter the file name: na na boo boo
 NA NA BOO BOO TO YOU - You have been punk'd!
 We are not encouraging you to put Easter Eggs in your programs; this is just an exercise.
 """
+try:
+    fname = input('Enter a filename: ')
+    lineCount = 0
+    fhandle = open(fname)
+    for line in fhandle:
+        if line.startswith('Subject:'):
+            lineCount += 1
+    print('There were ', lineCount, 'lines in ', fname)
+except:
+    if(fname == 'na na boo boo'):
+        print('NA NA BOO BOO TO YOU - You have been punk\'d!')
+    else:
+        print(fname, 'file cannot be opened')
+    quit()
+
+
+
